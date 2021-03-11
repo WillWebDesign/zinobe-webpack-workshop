22093e6463a60a59e30091d026400f449ef43460
William Bernal <willtf.wb@gmail.com>
2021-03-11 16:20:29 -0500

    Salidas paso 1

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/title.js", // The source module of our dependency graph
+  entry: {
+    main: "./src/title.js",
+    color: "./src/color.js"
+  },
   output: {
-    // Configuration of what we tell webpack to generate (here, a ./dist/main.js file)
-    filename: "main.js",
+    filename: "[name].js",
     path: path.resolve(__dirname, "dist")
   },
   module: {
