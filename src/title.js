18e5189691c85afec013f6f6ee52a41344759f49
William Bernal <willtf.wb@gmail.com>
2021-03-11 16:03:24 -0500

    Manejo de code assets

--- src/title.js
+++ src/title.js
@@ -1,14 +1,8 @@
 const { getRandomColor } = require("./color.js");
-const image = require("../assets/landscape.jpg").default;
 
 let changeCount = 0;
 
 const el = document.querySelector("h1");
-
-const img = document.createElement("img");
-img.src = image;
-
-el.parentNode.insertBefore(img, el.nextSibling);
 
 setInterval(() => {
   changeCount++;
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/title.js",
+  entry: "./src/title.js", // The source module of our dependency graph
   output: {
+    // Configuration of what we tell webpack to generate (here, a ./dist/main.js file)
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
   },
