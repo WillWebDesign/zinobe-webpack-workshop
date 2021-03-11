f298c62fc5f8ddf0685ff8a70612b9beb8f7eaf1
William Bernal <willtf.wb@gmail.com>
2021-03-11 15:22:57 -0500

    Base paso 1

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,9 @@
-module.exports = {};
+const path = require("path");
+
+module.exports = {
+  entry: "./src/title.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
