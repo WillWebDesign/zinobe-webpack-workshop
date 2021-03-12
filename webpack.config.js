c757303edd82fbef3361d0ab2417b0a80a623307
William Bernal <willtf.wb@gmail.com>
2021-03-12 13:30:49 -0500

    style paso 2.1

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
       },
       {
         test: /\.sass$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
+        use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader", "sass-loader"]
       }
     ]
   },
